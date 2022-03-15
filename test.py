c42fa2b64fc9b75adccabd4b98190448593dd6e8
GuriDonghan <snuffy@naver.com>
2022-03-15 18:19:20 +0900

    secondcommit

--- test.py
+++ test.py
@@ -1,1 +1,15 @@
-print("adfsds")
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+
+print(pd.__version__)
+print(sns.__version__)
+
+
+df = sns.load_dataset("mpg")
+
+df.head()
+df.tail()
+print(df.info())
